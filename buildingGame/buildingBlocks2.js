91b904cb30baf4cce81f8371ebcf0d3c120c4b73
moreoverthinking <greenkeychains@gmail.com>
2022-04-06 19:42:57 -0500

    try fixing lines between textures

--- buildingGame/buildingBlocks2.js
+++ buildingGame/buildingBlocks2.js
@@ -33,15 +33,15 @@ function loadTexture(url) {
 
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 
-    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-       gl.generateMipmap(gl.TEXTURE_2D);
-    } else {
+    //if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+       //gl.generateMipmap(gl.TEXTURE_2D);
+    //} else {
        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    }
+    //}
   }
   image.crossOrigin = "Anonymous";
   image.src = url;
