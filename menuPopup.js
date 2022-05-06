2fcddfcd1da839d728d93dfb6fbafdaa68fdb473
moreoverthinking <greenkeychains@gmail.com>
2022-05-06 00:32:28 -0500

    added background fade to pull out menu

--- menuPopup.js
+++ menuPopup.js
@@ -1,9 +1,24 @@
 function popup() {
   var menu = document.getElementById("menuPopup")
+  var fade = document.getElementById("bodyFade")
   if (menu.style.animationName == "open") {
     menu.style.animationName = "close";
+    fade.style.animationName = "fadeIn";
+
+    fade.style.pointerEvents = "none";
+    fade.onclick = null;
   }
   else {
     menu.style.animationName = "open";
+    fade.style.animationName = "fadeOut";
+
+    fade.style.pointerEvents = "auto";
+    fade.onclick = function() {
+      menu.style.animationName = "close";
+      fade.style.animationName = "fadeIn";
+
+      fade.style.pointerEvents = "none";
+      fade.onclick = null;
+    };
   }
 }
